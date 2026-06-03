be44e50c3f44d1058f213bd2e40e8859c6d25987
Nathan-Roberts123 <138929167+Nathan-Roberts123@users.noreply.github.com>
2026-06-03 08:18:06 +0200

    Update main.tf

--- DevOps_Project_2/Terraform/main.tf
+++ DevOps_Project_2/Terraform/main.tf
@@ -1,5 +1,6 @@
 data "aws_ami" "amazon-linux" {
   most_recent = true
+  owners = ["137112412989"]
 
   filter {
     name   = "name"
@@ -16,6 +17,7 @@ resource "aws_instance" "dev_machine" {
   ami = data.aws_ami.amazon-linux.id
   instance_type = "t2.micro"
   key_name = "euran-jenkins"
+  subnet_id = "subnet-037e65818dc0975b4"
 
   tags = {
     Environment = "dev"
